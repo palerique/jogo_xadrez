d1ea1c2eb60200bca690116612b90bfaa8939e89
Ricardo Maia <ricardo.jmm@gmail.com>
2020-05-16 16:42:48 -0300

    movimentacao peao

--- xadrez.h
+++ xadrez.h
@@ -14,22 +14,26 @@
 #define RAINHA_PRETO 'Q'
 #define ESPACO_VAZIO ' '
 
-#define LINHA_INFERIOR 1
-#define LINHA_SUPERIOR 8
-#define COLUNA_INFERIOR 1
-#define COLUNA_SUPERIOR 8
+#define LINHAS_TABULEIRO 8
+#define COLUNAS_TABULEIRO 8
 
-char tabuleiro[9][9]= { 
-  ESPACO_VAZIO, '1', '2', '3', '4', '5', '6', '7', '8',
-  '1', TORRE_BRANCO, CAVALO_BRANCO, BISPO_BRANCO, REI_BRANCO, RAINHA_BRANCO, BISPO_BRANCO, CAVALO_BRANCO, TORRE_BRANCO,
-  '2', PEAO_BRANCO, PEAO_BRANCO, PEAO_BRANCO, PEAO_BRANCO, PEAO_BRANCO, PEAO_BRANCO, PEAO_BRANCO, PEAO_BRANCO,
-  '3', ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO,
-  '4', ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO,
-  '5', ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO,
-  '6', ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO,
-  '7', PEAO_PRETO, PEAO_PRETO, PEAO_PRETO, PEAO_PRETO, PEAO_PRETO, PEAO_PRETO, PEAO_PRETO, PEAO_PRETO,
-  '8', TORRE_PRETO, CAVALO_PRETO, BISPO_PRETO, RAINHA_PRETO, REI_PRETO, BISPO_PRETO, CAVALO_PRETO, TORRE_PRETO
+char tabuleiro[LINHAS_TABULEIRO][COLUNAS_TABULEIRO]= { 
+  {TORRE_BRANCO, CAVALO_BRANCO, BISPO_BRANCO, REI_BRANCO, RAINHA_BRANCO, BISPO_BRANCO, CAVALO_BRANCO, TORRE_BRANCO},
+  {PEAO_BRANCO, PEAO_BRANCO, PEAO_BRANCO, PEAO_BRANCO, PEAO_BRANCO, PEAO_BRANCO, PEAO_BRANCO, PEAO_BRANCO},
+  {ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO},
+  {ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO},
+  {ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO},
+  {ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO, ESPACO_VAZIO},
+  {PEAO_PRETO, PEAO_PRETO, PEAO_PRETO, PEAO_PRETO, PEAO_PRETO, PEAO_PRETO, PEAO_PRETO, PEAO_PRETO},
+  {TORRE_PRETO, CAVALO_PRETO, BISPO_PRETO, RAINHA_PRETO, REI_PRETO, BISPO_PRETO, CAVALO_PRETO, TORRE_PRETO}
 };
+
+typedef struct {
+	int linha, coluna;
+} Posicao ;
+
+#define VERDADEIRO 1
+#define FALSO 0
 
 void exibir_tabuleiro();
 int validar_movimento(int linhaPeca, int colunaPeca, int linhaDestino, int colunaDestino, int jogadorAtual);
@@ -45,102 +49,164 @@ int movimentar_bispo_preto(int linhaPeca, int colunaPeca, int linhaDestino, int 
 int movimentar_rei(int linhaPeca, int colunaPeca, int linhaDestino, int colunaDestino);
 int movimentar_rainha(int linhaPeca, int colunaPeca, int linhaDestino, int colunaDestino);
 
-	void exibir_tabuleiro() {
-		int linha, coluna;
+void exibir_tabuleiro() {
+	int linha, coluna;
 
-		for (linha = 0; linha < 9; linha++) {
+	printf(" \t");
 
-			for (coluna = 0; coluna < 9; coluna++) {
-				printf("%3c", tabuleiro[linha][coluna]);
-			}
+	for (coluna = 0; coluna < COLUNAS_TABULEIRO; coluna++)
+		printf("%3d", coluna+1);
 
-			printf("\n");
+	printf("\n");
+
+	for (linha = 0; linha < LINHAS_TABULEIRO; linha++) {
+		printf("%d\t", linha+1);
+		for (coluna = 0; coluna < COLUNAS_TABULEIRO; coluna++) {
+			printf("%3c", tabuleiro[linha][coluna]);
 		}
+		printf("\n");
 	}
+}
 
-	int movimentar_peca(int linhaPeca, int colunaPeca, int linhaDestino, int colunaDestino, int jogadorAtual) {
-		int movimento_valido = 0;
+int movimentar_peca(int linhaPeca, int colunaPeca, int linhaDestino, int colunaDestino, int jogadorAtual) {
+	int movimento_valido = 0;
 
-		movimento_valido = validar_movimento(linhaPeca, colunaPeca, linhaDestino, colunaDestino, jogadorAtual);
+	linhaPeca--; colunaPeca--; linhaDestino--; colunaDestino--;
 
-		if (movimento_valido==1) {
-			printf("\nMovimento realizado com SUCESSO!!\n");
-			tabuleiro[linhaDestino][colunaDestino] = tabuleiro[linhaPeca][colunaPeca];
-			tabuleiro[linhaPeca][colunaPeca] = ESPACO_VAZIO;
-		} else {
-			printf("\nMovimento invAlido\n");
-		}
+	movimento_valido = validar_movimento(linhaPeca, colunaPeca, linhaDestino, colunaDestino, jogadorAtual);
+
+	if (movimento_valido==1) {
+		printf("\nMovimento realizado com SUCESSO!!\n");
+		tabuleiro[linhaDestino][colunaDestino] = tabuleiro[linhaPeca][colunaPeca];
+		tabuleiro[linhaPeca][colunaPeca] = ESPACO_VAZIO;
+	} else {
+		printf("\nMovimento invAlido\n");
+	}
 		
-		return 1;
-	}
+	return 1;
+}
 
-	int validar_movimento(int linhaPeca, int colunaPeca, int linhaDestino, int colunaDestino, int jogadorAtual) {
-		int movimento_valido = 0;
-		printf("%d %d %d %d", linhaPeca, colunaPeca, linhaDestino, colunaDestino);
+int validar_movimento(int linhaPeca, int colunaPeca, int linhaDestino, int colunaDestino, int jogadorAtual) {
+	int movimento_valido = 0;
+	printf("%d %d %d %d", linhaPeca, colunaPeca, linhaDestino, colunaDestino);
+		/*
 		if (linhaPeca < LINHA_INFERIOR || linhaPeca > LINHA_SUPERIOR || colunaPeca < COLUNA_INFERIOR
 				|| colunaPeca > COLUNA_SUPERIOR || linhaDestino < LINHA_INFERIOR || linhaDestino > LINHA_SUPERIOR
 				|| colunaDestino < LINHA_INFERIOR || colunaDestino > LINHA_SUPERIOR)
 			return movimento_valido;
+		*/
+	char pecaSelecionada = tabuleiro[linhaPeca][colunaPeca];
 
-		char pecaSelecionada = tabuleiro[linhaPeca][colunaPeca];
+	printf("\nPeça selecionada '%c'", pecaSelecionada);
+	printf("\nAlvo do movimento '%c'\n", tabuleiro[linhaDestino][colunaDestino]);
 
-		printf("\nPeça selecionada '%c'", pecaSelecionada);
-		printf("\nAlvo do movimento '%c'\n", tabuleiro[linhaDestino][colunaDestino]);
-
-		switch (pecaSelecionada) {
+	switch (pecaSelecionada) {
 		case PEAO_BRANCO:
-			    movimento_valido = movimentar_peao_branco(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
+			movimento_valido = movimentar_peao_branco(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
 			break;
 		case PEAO_PRETO:
-			    movimento_valido = movimentar_peao_preto(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
+		    movimento_valido = movimentar_peao_preto(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
 			break;
 		case TORRE_BRANCO:
-    			movimento_valido = movimentar_torre_branco(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
+    		movimento_valido = movimentar_torre_branco(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
 			break;
 		case TORRE_PRETO:
-    			movimento_valido = movimentar_torre_preto(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
+    		movimento_valido = movimentar_torre_preto(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
 			break;
 		case CAVALO_BRANCO:
-    			movimento_valido = movimentar_cavalo_branco(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
+    		movimento_valido = movimentar_cavalo_branco(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
 			break;
-		case CAVALO_PRETO:
-    			movimento_valido = movimentar_cavalo_preto(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
+		case CAVALO_PRETO:    			
+			movimento_valido = movimentar_cavalo_preto(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
 			break;
 		case BISPO_BRANCO:
-    			movimento_valido = movimentar_bispo_branco(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
+			movimento_valido = movimentar_bispo_branco(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
 			break;
 		case BISPO_PRETO:
-    			movimento_valido = movimentar_bispo_preto(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
+    		movimento_valido = movimentar_bispo_preto(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
 			break;
 		case REI_BRANCO:
-    			movimento_valido = movimentar_rei_branco(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
+    		movimento_valido = movimentar_rei_branco(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
 			break;
 		case REI_PRETO:
-    			movimento_valido = movimentar_rei_preto(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
+    		movimento_valido = movimentar_rei_preto(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
 			break;
 		case RAINHA_BRANCO:
-    			movimento_valido = movimentar_rainha_branco(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
+    		movimento_valido = movimentar_rainha_branco(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
 			break;
 		case RAINHA_PRETO:
-    			movimento_valido = movimentar_rainha_preto(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
+    		movimento_valido = movimentar_rainha_preto(linhaPeca, colunaPeca, linhaDestino, colunaDestino);
 			break;
+	}
+
+	return movimento_valido;
+}
+
+int posicaoExiste(Posicao posicao) {
+	int retorno=FALSO;
+
+	if(posicao.linha>=0 && posicao.linha<LINHAS_TABULEIRO && posicao.coluna>=0 && posicao.coluna<COLUNAS_TABULEIRO)
+		retorno=VERDADEIRO;
+
+	return retorno;
+}
+
+int existePeca(Posicao posicao) {
+	int retorno=VERDADEIRO;
+
+	if(tabuleiro[posicao.linha][posicao.coluna]==ESPACO_VAZIO)
+		retorno=FALSO;
+	return retorno;
+}
+
+void inicializarMatrizMovimentosPossiveis(int matrizMovimentosPossiveis[LINHAS_TABULEIRO][COLUNAS_TABULEIRO]) {
+	int i=0, j=0;
+	for(i=0; i<LINHAS_TABULEIRO; i++) {
+		for(j=0; j<COLUNAS_TABULEIRO; j++) {
+			matrizMovimentosPossiveis[i][j]=FALSO;
+		}
+	}
+}
+
+int movimentar_peao_branco(int linhaPeca, int colunaPeca, int linhaDestino, int colunaDestino) {
+		int matrizMovimentosPossiveis[LINHAS_TABULEIRO][COLUNAS_TABULEIRO];
+		inicializarMatrizMovimentosPossiveis(matrizMovimentosPossiveis);
+		
+		int sucesso = 0;
+		Posicao p;
+
+		p.linha=linhaPeca+1;
+		p.coluna=colunaPeca;
+		if (posicaoExiste(p) && !existePeca(p)) {
+			matrizMovimentosPossiveis[p.linha][p.coluna] = VERDADEIRO;
 		}
 
-		return movimento_valido;
-	}
+		p.linha=linhaPeca+2;
+		p.coluna=colunaPeca;
+		Posicao p2;
+		p2.linha=linhaPeca+1;
+		p2.coluna=colunaPeca;
 
-	int movimentar_peao_branco(int linhaPeca, int colunaPeca, int linhaDestino, int colunaDestino) {
-		int sucesso = 0;
-		printf("\nPeão Branco\n");
+		if (posicaoExiste(p) && !existePeca(p) && posicaoExiste(p2) && !existePeca(p2) ) { //&& getContagemMovimento() == 0) {
+				matrizMovimentosPossiveis[p.linha][p.coluna] = VERDADEIRO;
+		}
+/*		
+		if(posicao.getColuna()>0) {
+			p.setValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+			if (getTabuleiro().posicaoExiste(p) && ehPecaOponente(p)) {
+				matrizMovimentosPossiveis[p.getLinha()][p.getColuna()] = true;
+			}			
+		}
+		if(posicao.getColuna()<7) {
+			p.setValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+			if (getTabuleiro().posicaoExiste(p) && ehPecaOponente(p)) {
+				matrizMovimentosPossiveis[p.getLinha()][p.getColuna()] = true;
+			}
+		}
+*/		
 
-		if (colunaPeca != colunaDestino)
-			sucesso = 0;
-		else if (linhaPeca == 3 && (linhaDestino == 4 || linhaDestino == 5))
-			sucesso = 1;
-		else if (linhaPeca <=8 && linhaPeca > 3)
-			sucesso = 1;
 		return 1;
-	}
+}
 
 	int movimentar_peao_preto(int linhaPeca, int colunaPeca, int linhaDestino, int colunaDestino) {
 		int sucesso = 0;
