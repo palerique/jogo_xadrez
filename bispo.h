d36ebc3b17b84c048959f9b071329eccd43b582b
Ricardo Maia <ricardo.jmm@gmail.com>
2020-05-17 02:42:32 -0300

    movimentacao do bispo

--- bispo.h
+++ bispo.h
@@ -1,4 +1,51 @@
 void possivelMovimentoBispo(Peca *peca, Posicao posicaoDestino) {
-		printf("\nBispo Branco\n");
+	inicializarMatrizMovimentosPossiveis(peca);
+	Posicao p;
+		
+	// nw
+	p.linha=peca->posicao.linha-1;
+	p.coluna=peca->posicao.coluna-1;
+	while (posicaoExiste(p) && !existePeca(p)) {
+		peca->matrizMovimentosPossiveis[p.linha][p.coluna] = VERDADEIRO;
+		p.linha=peca->posicao.linha-1;
+		p.coluna=peca->posicao.coluna-1;
+	}
+	if (posicaoExiste(p) && ehPecaOponente(*peca, p)) {
+		peca->matrizMovimentosPossiveis[p.linha][p.coluna] = VERDADEIRO;
+	}
+		
+	// ne
+	p.linha=peca->posicao.linha-1;
+	p.coluna=peca->posicao.coluna+1;
+	while (posicaoExiste(p) && !existePeca(p)) {
+		peca->matrizMovimentosPossiveis[p.linha][p.coluna] = VERDADEIRO;
+		p.linha=peca->posicao.linha-1;
+		p.coluna=peca->posicao.coluna+1;
+	}
+	if (posicaoExiste(p) && ehPecaOponente(*peca, p)) {
+		peca->matrizMovimentosPossiveis[p.linha][p.coluna] = VERDADEIRO;
+	}
+		
+	// se
+	p.linha=peca->posicao.linha + 1;
+	p.coluna=peca->posicao.coluna + 1;
+	while (posicaoExiste(p) && !existePeca(p)) {
+		peca->matrizMovimentosPossiveis[p.linha][p.coluna] = VERDADEIRO;
+		p.linha=peca->posicao.linha + 1;
+		p.coluna=peca->posicao.coluna +1;
+	}
+	if (posicaoExiste(p) && ehPecaOponente(*peca, p)) {
+		peca->matrizMovimentosPossiveis[p.linha][p.coluna] = VERDADEIRO;
+	}
+		
+	// sw
+	p.linha=peca->posicao.linha + 1;
+	p.coluna=peca->posicao.coluna - 1;
+	while (posicaoExiste(p) && !existePeca(p)) {
+		peca->matrizMovimentosPossiveis[p.linha][p.coluna] = VERDADEIRO;
+		p.linha=peca->posicao.linha + 1;
+	}
+	if (posicaoExiste(p) && ehPecaOponente(*peca, p)) {
+		peca->matrizMovimentosPossiveis[p.linha][p.coluna] = VERDADEIRO;
+	}
 }
-
